b5c4387973a330d93751ac61c628ef1b997a8f9d
Pixoll <pixoll.gamer@gmail.com>
2024-03-06 14:38:38 -0300

    Retry with invalid input

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ def main():
     Game.printTitle()
 
     boardSize = toInt(getInput("Enter the board size: "))
-    if not boardSize or not Game.BoardSizeRange.count(boardSize):
+    while not boardSize or not Game.BoardSizeRange.count(boardSize):
         print(f"Board size must be in range [{min(Game.BoardSizeRange)}, {max(Game.BoardSizeRange)}]")
-        close(1)
+        boardSize = toInt(getInput("Enter the board size: "))
 
     shipsAmountRange = Game.shipsAmountRange(boardSize)
     shipsAmount = toInt(getInput("Enter the ships amount: "))
-    if not shipsAmount or not shipsAmountRange.count(shipsAmount):
+    while not shipsAmount or not shipsAmountRange.count(shipsAmount):
         print(f"Ships amount must be in range [{min(shipsAmountRange)}, {max(shipsAmountRange)}]")
-        close(1)
+        shipsAmount = toInt(getInput("Enter the ships amount: "))
 
     firstPlayer = PlayerType.getByName(getInput("Enter who starts first: ").capitalize())
-    if firstPlayer == None:
+    while firstPlayer == None:
         print(f"First player must be either {', '.join(map(
             lambda a: f'or {a}' if PlayerType.names()[-1] == a else a, PlayerType.names()
         ))}")
-        close(1)
+        firstPlayer = PlayerType.getByName(getInput("Enter who starts first: ").capitalize())
 
     game = Game(boardSize, shipsAmount, firstPlayer)
     game.getShipPlacements()
